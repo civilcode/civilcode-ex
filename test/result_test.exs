6e7c68b835bbf16ebf9199fcaf6aed90cf35ac18
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-21 09:27:35 -0400

    test: Remove unused Result.unwrap/1 test

--- test/result_test.exs
+++ test/result_test.exs
@@ -3,19 +3,6 @@ defmodule CivilCode.ResultTest do
 
   alias CivilCode.Result
 
-  test "unwrapping a result" do
-    assert :payload == Result.unwrap({:ok, :payload})
-    assert :payload == Result.unwrap({:error, :payload})
-  end
-
-  test "unwrapping success result" do
-    assert :payload == Result.unwrap!({:ok, :payload})
-  end
-
-  test "unwrapping error result" do
-    assert_raise(RuntimeError, fn -> Result.unwrap!({:error, :payload}) end)
-  end
-
   test "combining a result" do
     assert {:ok, [:foo, :bar]} == Result.combine({:ok, :foo}, {:ok, :bar})
     assert {:ok, [:foo, :bar]} == Result.combine({:ok, :foo}, {:ok, [:bar]})
