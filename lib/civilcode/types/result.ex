e7e042854522560f6d084efd2819a1a5f6d997df
Nicholas Henry <nicholas.henry@gmail.com>
2019-09-20 15:10:36 -0400

    docs: Document the functions of the result type
    Closes #37

--- lib/civilcode/types/result.ex
+++ lib/civilcode/types/result.ex
@@ -5,18 +5,26 @@ defmodule CivilCode.Result do
 
   @type ok(any) :: {:ok, any}
   @type error(any) :: {:error, any}
+
+  @typedoc "the two-track type"
   @type t(a, b) :: ok(a) | error(b)
 
-  @spec unwrap!({:ok, any}) :: any
+  @spec unwrap!(ok(any)) :: any | no_return
   def unwrap!({:ok, payload}), do: payload
 
   def unwrap!({:error, value}) do
     raise "Unexpected error tuple: #{inspect(value)}"
   end
 
+  @doc """
+  Convert a single value into a two-track result.
+  """
   @spec ok(any) :: ok(any)
   def ok(result), do: {:ok, result}
 
+  @doc """
+  Convert a single value into a two-track result.
+  """
   @spec error(any) :: error(any)
   def error(result), do: {:error, result}
 
@@ -24,11 +32,17 @@ defmodule CivilCode.Result do
   def error?({:error, _}), do: true
   def error?(_), do: false
 
+  @doc """
+  Convert a one-track function into a two-track function.
+  """
   @spec map(t(any, any), (any -> any)) :: t(any, any)
   def map({:ok, payload}, func), do: ok(func.(payload))
   def map(result, _func), do: result
 
-  @spec combine(t(any, any), t(any, any)) :: {:ok, [any()]} | {:error, [any()]}
+  @doc """
+  Combine two result types.
+  """
+  @spec combine(t(any, any), t(any, any)) :: ok([any]) | error([any])
   def combine({:ok, lhr}, {:ok, rhr}) do
     {:ok, List.flatten([lhr, rhr])}
   end
